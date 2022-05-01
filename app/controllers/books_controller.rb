b92632da78eac48bfaee72b371bfd91ab21d52d2
mizu-hisa <101858095+mizu-hisa@users.noreply.github.com>
2022-05-01 07:57:48 +0000

    [Finish]応用課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,21 @@
 class BooksController < ApplicationController
 
+
   def show
     @book = Book.find(params[:id])
+    @user=@book.user
+    @book_new=Book.new
   end
 
   def index
     @books = Book.all
+    @book=Book.new
+    @user=current_user
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -31,9 +37,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
@@ -42,5 +48,15 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
+
+  before_action :correct_user, only: [:edit, :update]
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   end
 
   def index
+    @user=current_user
     @users = User.all
     @book = Book.new
   end
@@ -33,7 +34,8 @@ class UsersController < ApplicationController
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
-      redirect_to user_path(current_user)
+    redirect_to user_path(current_user)
     end
   end
+
 end
